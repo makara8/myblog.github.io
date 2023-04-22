15e0fc3774793b7cf9c6226d1cec093b0c05bf70
makara8 <130232333+makara8@users.noreply.github.com>
2023-04-22 17:11:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg bg-light navbar-light sticky-top" style="background-color: white !important;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="seo.jpg" alt="Logo" style="width:150px;" >
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html" style="color: black;">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html" style="color: black;">Sign up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html" style="color: black;">login</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2 myInput" type="text" placeholder="Search" >
+          <button class="btn btn-primary" type="button">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  
+</body>
+</html>
